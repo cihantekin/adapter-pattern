8acc2588e9d21683512c2c71035f044709dc9139
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-16 21:15:31 +0100

    modern stuff using record

---
+++ adapter-pattern/ModernStuff/Footballer.cs
@@ -0,0 +1,18 @@
+﻿namespace adapter_pattern.ModernStuff
+{
+    public record Footballer
+    {
+        public string Team { get; set; } 
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Position Position { get; set; }
+    }
+
+    public enum Position
+    {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Striker
+    }
+}
